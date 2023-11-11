eb05cfda682cc588f84653f043dddc3084ebe3ef
OyedunOye <oyesinaoyedun@yahoo.com>
2023-11-11 20:40:52 +0100

    updated CTA component

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -3,7 +3,15 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='gtp3__cta'>
+      <div className='gtp3__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today and start exploring the endless possibilities.</h3>
+      </div>
+      <div className='gtp3__cta-btn'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
